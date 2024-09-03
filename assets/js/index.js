97b670a8afc4db2ce68d78c0f8c04e7e6aded91e
Alaa-Mostafaa <alan.mustafa.2002@gmail.com>
2024-09-03 15:30:17 +0200

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,55 @@
-
 var images=document.querySelectorAll('.image-container');
 var dots=document.querySelectorAll('.fa-circle');
+var currentIndex = 0;
 
+// show first image only
 function ShowSlides(){
-    for(var i=0;i<dots.length;i++){
-        dots[i+1].style.color='grey';
         dots[0].style.color='blue';
-        images[i+1].style.display='none';
-    }
+        images[0].style.display='flex';
 }
 ShowSlides()
 
-// click on dots
-function dot(n){
-for(var i=0;i<dots.length;i++){
-    dots[i].style.color='grey'
-    images[i].style.display='none'
-}
-dots[n].style.color='blue'
-images[n].style.display='flex'
-}
-
-// click on arrows
-function nextImage(number,index){
-    for(var i=0;i<dots.length;i++){
-        dots[i].style.color='grey'
-        images[i].style.display='none'
+// Function to update the slides and dots
+function updateSlider(index) {
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].style.color = 'grey';
+        images[i].style.display = 'none';
     }
-    dots[index+number].style.color='blue'
-    images[index+number].style.display='flex'
+    dots[index].style.color = 'blue';
+    images[index].style.display = 'flex';
 }
 
-images.addEventListener('touchmove', (e) => {
-    const currentX = e.touches[0].clientX;
-    const diff = currentX - startX;
-    sliderWrapper.style.transform = `translateX(${currentTranslate + diff}px)`;
-});
+// Function for dot navigation
+function dot(n) {
+    currentIndex = n; // Update the current index
+    updateSlider(n);
+}
+
+// Function for arrow navigation
+function nextImage(number, index) {
+    currentIndex = (number + index)
+    updateSlider(currentIndex);
+}
+
+// Function to auto-cycle through images every 2 seconds
+function startAutoSlide() {
+    interval=setInterval(function() {
+        currentIndex = (currentIndex + 1) 
+        if(currentIndex > 3){
+            currentIndex=0
+        }else if(currentIndex < 0){
+            currentIndex=3;
+        }
+        updateSlider(currentIndex);
+    }, 2500); 
+}
+
+startAutoSlide();
+
+function stop(){
+    clearInterval(interval)
+}
+
+function m(event){
+    updateSlider(0)
+}
